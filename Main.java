2f58c28cc66c5dd3e311fef80057c6e7295ab747
carlagochuico <gochuicocarlacassandra.scc@gmail.com>
2025-08-22 01:44:57 +0000

    SeatWork 5

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Gochuico");
+        int countdown = 3;
+        
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy Birthday!!");
     }
+
 }
