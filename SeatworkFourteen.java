bd4c1a6ea4643b85aa293701878af94efa122680
carlagochuico <gochuicocarlacassandra.scc@gmail.com>
2025-10-25 14:14:05 +0800

    Update SeatworkFourteen.java

--- SeatworkFourteen.java
+++ SeatworkFourteen.java
@@ -1,17 +1,35 @@
-public static void main (String args[l) {
-Motorcycle m = new Motorcycle() ;
-m. make = "Yamaha RZ350" ;
-m. color = "yellow";
-System.out println ("Calling showAtts...") ;
-m. showAtts ( );
-System.out.println("-- .println("--- - --- -") ;
-System.out println ("Starting engine...") ;
-m. startEngine ();
-System.out. println("--
--") ;
-System.out println ("Calling showAtts...") ;
-m. showAtts () ;
-System.out println("--
--") ;
-System.out println("Starting engine...");
-m. startEngine();
+public class SeatworkFourteen
+public static void main(String[]args) {
+    Motorcycle m= new Motorcycle();
+    m.make= "Yamaha RZ350";
+    m.color="yellow";
+
+    System.out.println("Calling showAtts...");
+    m.showAtts();
+    System.out.println("-------------");
+
+    System.out.println("Starting engine...");
+    m.startEngine();
+    System.out.println("-------------");
+
+    System.out.println("Calling showAtts...");
+    m.showAtts();
+    System.out.println("-------------");
+
+    System.out.println("Starting engine...");
+    m.startEngine();
+}
+
+
+  // Assuming Motorcycle class exist somewhere
+     class Motorcycle {
+     String make;
+     String color;
+     public void showAtts() {
+     System.out.println("This motorcycle is a"+ color + ""+ make);
+    }
+
+     public void startEngine() {
+     System.out.println("The engine is now running");
+    }
+  }
